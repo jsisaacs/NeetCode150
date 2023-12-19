41895c053e4f44a137980b63c6ae12d9ed3018d0
Joshua Isaacson <jsisaacson98@gmail.com>
2023-12-19 11:38:26 -0500

    completed two sum 2 - sorted input

--- src/main/java/twoPointers/TwoPointers.java
+++ src/main/java/twoPointers/TwoPointers.java
@@ -25,4 +25,32 @@ public class TwoPointers {
         }
         return true;
     }
+
+    /**
+     * Runtime: O(n), Space O(1)
+     * @param nums 1-indexed sorted in non-decreasing order
+     * @param t target, there is exactly one solution
+     * @return indices of the 2 numbers that add up to t (1-indexed)
+     */
+    public static int[] twoSumSortedInput(int[] nums, int t) {
+        int l = 0;
+        int r = nums.length - 1;
+        int[] answer = new int[2];
+        while (l <= r) {
+            int numL = nums[l];
+            int numR = nums[r];
+            if (numL + numR == t) {
+                //answer is 1-indexed
+                answer[0] = l + 1;
+                answer[1] = r + 1;
+                break;
+            }
+            if (numL + numR > t) {
+                r--;
+                continue;
+            }
+            l++;
+        }
+        return answer;
+    }
 }
